394914d27aff96e2328c2744064728b477fcb5de
pixel-fabian <82926714+pixel-fabian@users.noreply.github.com>
2022-04-09 22:02:35 +0200

    :sparkles: ADD pagination to category page

--- src/category.php
+++ src/category.php
@@ -10,11 +10,49 @@
 
     <section class="teaser-section">
       <?php
-        while (have_posts()) : the_post();
+        if ( get_query_var( 'paged' ) ) {
+          $paged = get_query_var( 'paged' );
+        } elseif ( get_query_var( 'page' ) ) {
+          $paged = get_query_var( 'page' );
+        } else {
+          $paged = 1;
+        }
+        $category = get_category(get_query_var('cat'))->name;
+ 
+        $posts = new WP_Query(array(
+          'category_name' => $category,
+          'posts_per_page' => 9,
+          'paged' => $paged,
+        ));
+
+        while ($posts->have_posts()) : $posts->the_post();
           get_template_part('template-parts/teaser');
-        endwhile; 
-      ?>   
+        endwhile;
+        
+      ?>
     </section>
-    <?php endif; ?>
+
+      <?php $total_pages = $posts->max_num_pages;
+      if ($total_pages > 1) { ?>
+
+      <div class="pagination">
+
+        <?php $current_page = max(1, $paged);
+
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+            'prev_text'    => __('« Vorherige Seite'),
+            'next_text'    => __('Nächste Seite »'),
+        ));
+
+      }
+    ?> </div>
+    <?php wp_reset_postdata(); ?>
+    <?php endif; ?> 
+
   </main>
+
 <?php get_footer(); ?>
